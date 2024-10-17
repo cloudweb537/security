1a5d5259a40ba07d00b89b5353621a6d24b40a7a
mahes <maheshbabubadukuri@gmail.com>
2024-10-17 11:48:00 +0530

    updated code for creating different session ids

--- demo-of-security/src/main/java/com/example/demo/of/security/config/SecurityConfig.java
+++ demo-of-security/src/main/java/com/example/demo/of/security/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.demo.of.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +15,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer->customizer.disable());
+        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+       //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+//Creates the different session
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
 
     }
